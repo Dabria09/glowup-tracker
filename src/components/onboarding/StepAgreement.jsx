2bc5387b30444d75ca1463db08494749d7b5cd38
Bria <rasheleria@gmail.com>
2026-06-08 04:55:50 +0000

    File changes

--- src/components/onboarding/StepAgreement.jsx
+++ src/components/onboarding/StepAgreement.jsx
@@ -233,7 +233,6 @@ export default function StepAgreement({ acceptTOS, setAcceptTOS, acceptConduct, 
             type="button"
             onClick={(e) => {
               console.log('🟢 INLINE onClick fired');
-              alert('BUTTON CLICKED!');
               handleSubmit(e);
             }}
             className="flex-1 bg-primary hover:bg-primary/90 text-white relative z-10"
